bb6b633aff69278aea2c8cf0a6b3bfb3aeb91230
vuonglestudent <vuongvaba1v2@gmail.com>
2020-11-28 20:42:48 +0700

    add data manager-user.component

--- argon-design-system-angular-master/src/app/manager-user/manager-user.component.ts
+++ argon-design-system-angular-master/src/app/manager-user/manager-user.component.ts
@@ -1,3 +1,5 @@
+import { User } from './../Models/Models';
+import { UsersService } from './../service/users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  Loading = false;
+
+  Favoritors: User[] = new Array();
+  FavoritePage: any = {
+    index: 1,
+    size: 7,
+    total: 0,
+    current: 1,
+    position: 1
+  };
 
   ngOnInit(): void {
+    this.updatePagingNumber(1);
+    this.getFavoritors();
   }
 
+  setActiveUser = (e) => {
+    var target = e.target;
+    console.log(target.className)
+    if (target.className === "media-body btn-flip") {
+      target.className = "media-body btn-flip active-btn-flip"
+    } else {
+      target.className = "media-body btn-flip"
+    }
+  }
+
+  getFavoritors() {
+    this.Loading = true;
+    console.log('get page: ' + this.FavoritePage.index);
+    this.usersService.GetFavoritest(this.FavoritePage.index, this.FavoritePage.size)
+      .then(response => {
+        this.Loading = false;
+        this.Favoritors = response.data;
+        this.FavoritePage.total = response.pageTotal;
+
+        // this.Favoritors.forEach(element => {
+        //   this.imageService.getImageByUserId(element.id)
+        //     .then(data => {
+        //       const imageUser = {} as ImageUser
+        //       imageUser.id = element.id;
+        //       imageUser.images = data;
+        //       this.imageUsers.push(imageUser)
+        //     })
+        //     .catch(error => {
+        //       this.alertService.clear();
+        //       this.alertService.error("Có lỗi khi tải hình ảnh!");
+        //     })
+        // });
+      })
+      .catch(error => {
+        //this.Loading = false;
+        // this.alertService.clear();
+        // this.alertService.error("Lỗi server, vui lòng thử lại sau!", this.options);
+      })
+
+  }
+  updatePagingNumber(page: number) {
+
+    console.log(this.FavoritePage.total)
+    this.FavoritePage.index = page;
+    if (page == 1) {
+      this.FavoritePage.position = 1;
+      this.FavoritePage.current = 2;
+    }
+    else if (page == this.FavoritePage.total) {
+      this.FavoritePage.position = 3;
+      this.FavoritePage.current = page - 1;
+    }
+    else {
+      this.FavoritePage.position = 2;
+      this.FavoritePage.current = page;
+    }
+  };
+
+  paging(index: number) {
+    console.log('paging to page ' + index.toString());
+
+    this.updatePagingNumber(index);
+    this.getFavoritors();
+  }
+  previousPage() {
+    if (this.FavoritePage.index == 1) {
+      return
+    }
+    this.updatePagingNumber(this.FavoritePage.index - 1);
+    this.getFavoritors();
+  }
+  nextPage() {
+    if (this.FavoritePage.index == this.FavoritePage.total) {
+      return;
+    }
+    this.updatePagingNumber(this.FavoritePage.index + 1);
+    this.getFavoritors();
+  }
 }
